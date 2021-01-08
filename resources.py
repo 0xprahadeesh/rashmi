64ad02bb428d77b6df6e6cb6a83d3e9bd1dc95d1
chill tamizha <43850019+chilltamizha@users.noreply.github.com>
2021-01-09 00:32:31 +0530

    Update resources.py

--- resources.py
+++ resources.py
@@ -26,5 +26,45 @@ subprocess.check_call([sys.executable, '-m', 'pip', 'install',
 'pypng'])
 
 subprocess.check_call([sys.executable, '-m', 'pip', 'install', 
+'pytest-shutil'])import sys
+import subprocess
+
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 
+'requests'])
+
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 
+'pyttsx3'])
+
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 
+'datetime'])
+
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 
+'speechrecognition'])
+
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 
+'wikipedia'])
+
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 
+'pyjokes'])
+
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 
+'pyqrcode'])
+
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 
+'pypng'])
+
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 
 'pytest-shutil'])
+
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 
+'pillow'])
+
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 
+'wheel'])
+
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 
+'pipwin'])
+
+subprocess.check_call([sys.executable, '-m', 'pipwin', 'install', 
+'pyaudio'])
 
