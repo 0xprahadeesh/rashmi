ef9484d4308392f7ccd4efa19e9bf75de16c1f8c
chill tamizha <43850019+chilltamizha@users.noreply.github.com>
2021-01-09 00:43:17 +0530

    Update qrcode.py

--- qrcode.py
+++ qrcode.py
@@ -3,12 +3,12 @@ import png
 
 url='https://instagram.com/chill_tamizha'
 p = pyqrcode.create(url)
-p.png('C:/Users/91860/Documents/Qr.png',scale=10)
+p.png('C:/Users/Qr.png',scale=10)
 
 def profile():
     from PIL import Image 
   
-    im = Image.open(r"C:/Users/91860/Documents/Qr.png")  
+    im = Image.open(r"C:/Users/Qr.png")  
     im.show()
 
 profile()
